d217538f39a5f04b8b94852b38b141db2187e601
Beli <unsis.b@gmail.com>
2019-11-27 13:15:48 -0600

    eliminar error de practica controller

--- src/java/com/clinica/controller/PracticaController.java
+++ src/java/com/clinica/controller/PracticaController.java
@@ -5,47 +5,12 @@
  */
 package com.clinica.controller;
 
-import com.clinica.entity.Practica;
-import com.clinica.service.IPracticaService;
-import java.util.List;
-import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.view.ViewScoped;
+
 
 /**
  *
  * @author labtw08
  */
-@ManagedBean(name ="vistaUsuarios")
-@ViewScoped
+
 public class PracticaController {
-    private List<Practica> Practicas;
-    private Practica practica;
-    
-    @ManagedProperty("#{practicaService}")
-    private IPracticaService service;
-
-    public Practica getPractica() {
-        return practica;
-    }
-
-    public void setPractica(Practica practica) {
-        this.practica = practica;
-    }
-    
-    @PostConstruct
-    public void init() {
-        this.Practicas=service.obtenerPracticas();
-        practica = new Practica();
-    }
-
-    public List<Practica> getPracticas() {
-        return Practicas;
-    }
-
-    public void setService(IPracticaService service) {
-        this.service = service;
-    }
-    
 }
